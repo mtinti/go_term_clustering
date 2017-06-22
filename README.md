b02032f1db669c692c26240070921b6d22bb3546
mtinti <michele.tinti@gmail.com>
2017-06-22 07:41:23 +0100

    modify readme

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # go term clustering
-function do determine the cutting distance of a dendogram
-based on go term counting
+## functions do determine the cutting distance of a dendogram based on go term counting
+
+### to run in this order:
+- prepare_data.py
+- find_enrichment.ipynb
+- find_pearson.ipynb
+- make_figure.py
+
+
+
--- make_figure.py
+++ make_figure.py
@@ -14,11 +14,32 @@ import statsmodels.api as sm
 lowess = sm.nonparametric.lowess
 
 combined_go = pd.DataFrame.from_csv('reduced_go_terms.csv')
+for col in combined_go.columns:
+    combined_go[col]=combined_go[col]/combined_go[col].max()
+    
 combined_random_go = pd.DataFrame.from_csv('reduced_random_go_terms.csv')
+for col in combined_random_go.columns:
+    combined_random_go[col]=combined_go[col]/combined_random_go[col]
+
+combined_random_go.columns = [n+'f' for n in combined_random_go.columns]
 combined_pr= pd.DataFrame.from_csv('reduced_pearson.csv')
-print combined_go.head()
-print combined_pr.head()
-print combined_random_go.head()
+#print combined_go.head()
+#print combined_pr.head()
+#print combined_random_go.head()
+
+
+combined_go.plot()
+combined_random_go.plot()
+combined_pr.plot()
+
+combined = pd.concat([combined_go,combined_random_go,combined_pr],1)
+combined = combined.fillna(0)
+print combined.head()
+combined.plot()
+plt.xlim(0,5)
+
+
+
 
 '''
 #combined_norm = combined[['CC','MF','BP']]
